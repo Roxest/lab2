356166b0b8430d30e20da0080afd7647e3d6bf9c
Roxest <thammarin82@gmail.com>
2021-07-11 09:23:35 +0700

    almost done

--- q5.js
+++ q5.js
@@ -1,5 +1,32 @@
 const mafiaWar = (mafias, fights) => {
   /* Your code here */
+  let edmaf = mafias
+  let ring = []
+  let queue = fights
+  while (queue.length != 0){
+    ring.push(Object.assign({},edmaf[queue[0]-1]))
+    if (ring.length == 2){
+      if(ring[0].power > ring[1].power){
+        edmaf[ring[0].id-1].power = ring[0].power+ring[1].power
+        edmaf[ring[0].id-1].members.push(ring[1].id)
+        ring = []
+      }else{
+        edmaf[ring[1].id-1].power = ring[1].power+ring[0].power
+        edmaf[ring[1].id-1].members.push(ring[0].id)
+        ring = []
+      }
+    }
+    queue.shift()
+  }
+  let strongest = []
+  strongest.push(Object.assign({},edmaf[0]))
+  for(let i = 0;i < edmaf.length;i++){
+    if(strongest[0].power < edmaf[i].power){
+      strongest.push(Object.assign({},edmaf[i]))
+      strongest.shift()
+    }
+  }
+  return strongest
 }
 
 //Test case
